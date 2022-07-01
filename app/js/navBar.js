b53f993e4530dc8e64043ba7b03871badb164a2f
Adrian Hartanto <adrianhartanto0312@gmail.com>
2022-07-01 15:18:57 +0700

    Finish Navbar

---
+++ app/js/navBar.js
@@ -0,0 +1,18 @@
+const menuButton = document.getElementById("nav-menu"),
+  navLinks = document.getElementById("nav-links"),
+  navClose = document.getElementById("nav-close"),
+  navOverlay = document.getElementById("nav-overlay");
+
+function showNavLinks() {
+  navOverlay.classList.add("nav-show");
+  navLinks.classList.add("nav-show");
+}
+
+function closeNavLinks() {
+  navOverlay.classList.remove("nav-show");
+  navLinks.classList.remove("nav-show");
+}
+
+menuButton.addEventListener("click", showNavLinks);
+navClose.addEventListener("click", closeNavLinks);
+navOverlay.addEventListener("click", closeNavLinks);
